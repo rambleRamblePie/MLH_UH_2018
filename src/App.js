b7d303f419b625fdf9a1664818461e5ba9b2b091
jaredtalbert <jjtalbert@uh.edu>
2018-11-03 18:24:36 -0500

    Update stylesheet

--- src/App.js
+++ src/App.js
@@ -38,39 +38,40 @@ class App extends Component {
           <p>Hi, tell us where you're coming from and where you're going</p>
           <form>
               <div class="form">
-                  <div>
+                  <div class="topForm">
                       <label htmlFor="origin">Origin:</label>
                       <input type="text" id="origin" name="origin_location"></input>
                   </div>
-                  <div>
+                  <div class="topForm">
                       <label htmlFor="destination">Destination:</label>
                       <input type="text" id="destination" name="destination_location"></input>
                   </div>
 
-              <div>
-                  <div>
-                      <label htmlFor="departureDate">Departure Date:</label>
-                      <DatePicker
-                          selected={this.state.startDate}
-                          selectsStart
-                          startDate={this.state.startDate}
-                          endDate={this.state.endDate}
-                          onChange={this.handleChangeStart}
-                      />
-                  </div>
-                  <div>
-                      <label htmlFor="returnDate">Return Date:</label>
-                      <DatePicker id={"returnDateSelector"}
-                          selected={this.state.endDate}
-                          selectsEnd
-                          startDate={this.state.startDate}
-                          endDate={this.state.endDate}
-                          onChange={this.handleChangeEnd}
-                          // excludeDates={[moment(), moment().to(this.startDate)]}
-                          // placeholderText="Select a date other than today or yesterday"
-                      />
-                  </div>
+                  <p></p>
+              <div class="bottomForm">
+                  <label htmlFor="departureDate">Departure Date:</label>
+                  <DatePicker
+                      selected={this.state.startDate}
+                      selectsStart
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate}
+                      onChange={this.handleChangeStart}
+                  />
               </div>
+              <div class="bottomForm">
+                  <label htmlFor="returnDate">Return Date:</label>
+                  <DatePicker id={"returnDateSelector"}
+                      selected={this.state.endDate}
+                      selectsEnd
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate}
+                      onChange={this.handleChangeEnd}
+                      // excludeDates={[moment(), moment().to(this.startDate)]}
+                      // placeholderText="Select a date other than today or yesterday"
+                  />
+              </div>
+
+
               <div class="button">
                   <button type="submit"> Find Lowest Prices</button>
               </div>
