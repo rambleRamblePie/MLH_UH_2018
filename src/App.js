f363f64ff7f447562f7017538a87aafcd1d803b3
Jared Talbert <jaredtalbert@users.noreply.github.com>
2018-11-03 16:32:53 -0500

    React-ified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React, { Component } from 'react';
+
+import DatePicker from "react-datepicker";
+import moment from "moment";
+
 import logo from './logo.svg';
 import './App.css';
 
+import "react-datepicker/dist/react-datepicker.css";
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: moment(),
+            endDate: moment()
+        };
+        this.handleChangeStart = this.handleChangeStart.bind(this);
+        this.handleChangeEnd = this.handleChangeEnd.bind(this);
+    }
+
+    handleChangeStart(date) {
+        this.setState({
+            startDate: date
+
+        });
+    }
+    handleChangeEnd(date) {
+        this.setState({
+            endDate: date
+        });
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>Hi, tell us where you're coming from and where you're going</p>
           <form>
+              <div class="form">
+                  <div>
+                      <label htmlFor="origin">Origin:</label>
+                      <input type="text" id="origin" name="origin_location"></input>
+                  </div>
+                  <div>
+                      <label htmlFor="destination">Destination:</label>
+                      <input type="text" id="destination" name="destination_location"></input>
+                  </div>
+
               <div>
-                  <label htmlFor="origin">Origin:</label>
-                  <input type="text" id="origin" name="origin_location"></input>
-              </div>
-              <div>
-                  <label htmlFor="destination">Destination:</label>
-                  <input type="text" id="destination" name="destination_location"></input>
+                  <div>
+                      <label htmlFor="departureDate">Departure Date:</label>
+                      <DatePicker
+                          selected={this.state.startDate}
+                          selectsStart
+                          startDate={this.state.startDate}
+                          endDate={this.state.endDate}
+                          onChange={this.handleChangeStart}
+                      />
+                  </div>
+                  <div>
+                      <label htmlFor="returnDate">Return Date:</label>
+                      <DatePicker id={"returnDateSelector"}
+                          selected={this.state.endDate}
+                          selectsEnd
+                          startDate={this.state.startDate}
+                          endDate={this.state.endDate}
+                          onChange={this.handleChangeEnd}
+                          // excludeDates={[moment(), moment().to(this.startDate)]}
+                          // placeholderText="Select a date other than today or yesterday"
+                      />
+                  </div>
               </div>
               <div class="button">
                   <button type="submit"> Find Lowest Prices</button>
+              </div>
               </div>
           </form>
         </header>
