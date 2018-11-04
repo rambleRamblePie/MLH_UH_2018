37cf5e27f712ec971ed577c3cd61e9f8ec4a4a7c
Joseph Park <vapor47@gmail.com>
2018-11-04 09:09:48 -0600

    fixed stuff

---
+++ CodeRED project/index.js
@@ -0,0 +1,8 @@
+function toggleRoundTrip(){
+    var x = document.getElementById("returnDateDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
